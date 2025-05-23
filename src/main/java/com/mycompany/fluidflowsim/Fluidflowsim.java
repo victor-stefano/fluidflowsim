7ff1d9b591df3c798be5f9f12409d06c369dd939
Lucas Alberto dos Santos Carvalho - 52217292 <52217292@ulife.com.br>
2025-05-23 19:48:26 -0300

    initial commit

---
+++ src/main/java/com/mycompany/fluidflowsim/Fluidflowsim.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fluidflowsim;
+
+/**
+ *
+ * @author 52217292
+ */
+public class Fluidflowsim {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
